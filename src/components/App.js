76a17c7ecfb549579d455793662d172a09f69824
Minh Tran <93815818+Conabot@users.noreply.github.com>
2023-06-12 13:49:05 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
+import { ta } from "date-fns/locale";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [categories, setCategories] = useState(CATEGORIES);
+  const [selectedCategory, setSelectedCategory] = useState ("All");
+  //const [filteredTasksCategory, setFilteredTasksCategory] = useState(tasks);
+
+  console.log(selectedCategory)
+ //console.log(filteredTasksCategory)
+  const handleDeleteTask = (deleteTask) => {
+    const filteredTasks = tasks.filter((task) => deleteTask.text !== task.text);
+    setTasks(filteredTasks)
+  }
+  
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask])
+  }
+
+  const handleCategoryClick = (category) => {
+    
+    setSelectedCategory(category)
+    
+    }
+
+  const filteredTasksCategory= tasks.filter((task) => {
+  if (selectedCategory === "All") {
+    return task
+  } else{
+    return task.category === selectedCategory}})
+
+
+ 
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={categories} 
+                      handleCategoryClick={handleCategoryClick} 
+                      selectedCategory={selectedCategory}/>
+      <NewTaskForm categories={categories} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={filteredTasksCategory} handleDeleteTask={handleDeleteTask}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { TASKS } from "../data";
 
-function Task() {
+function Task({task, text, category, handleDeleteTask}) {
+
+  const handleDelete = () => {
+    handleDeleteTask(task)
+  }
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={handleDelete} className="delete">X</button>
     </div>
   );
 }

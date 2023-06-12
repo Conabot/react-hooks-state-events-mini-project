c05a2386ee42ae3070a95c09e2d0167b1401bb6d
Minh Tran <93815818+Conabot@users.noreply.github.com>
2023-06-12 14:05:47 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-import { ta } from "date-fns/locale";
-console.log("Here's the data you're working with");
+//import { ta } from "date-fns/locale";
+//console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
